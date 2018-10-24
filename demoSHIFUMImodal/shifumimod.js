f112180f4930bf84b0b0a2c25c40636918bc00b2
dclkent1parker <43646760+dclkent1parker@users.noreply.github.com>
2018-10-24 15:53:52 +0200

    add mario theme

--- demoSHIFUMImodal/shifumimod.js
+++ demoSHIFUMImodal/shifumimod.js
@@ -1,4 +1,8 @@
 
+
+
+
+
 function game(){
 
     var rock = document.getElementById("rock");
@@ -15,10 +19,64 @@ function game(){
     var lose = document.getElementById("lose");
     var won = document.getElementById("won");
     var equal = document.getElementById("equal");
-    var newgame = document.getElementById("reset");
+    var newgame = document.getElementById("replay");
     var imageArray = [rock2, paper2, scissors2];
     var choiceIA;
 
+    function changeTheme(){
+        var theme1= document.getElementById("theme1");
+        var theme2= document.getElementById("theme2");
+        var styleCSS= document.getElementById("stylesheet");
+        var rock0= document.getElementById("rock0");
+        var paper0= document.getElementById("paper0");
+        var scissors0= document.getElementById("scissors0");
+        var tooltip1= document.getElementById("tooltip1");
+        var tooltip2= document.getElementById("tooltip2");
+        var tooltip3= document.getElementById("tooltip3");
+
+        theme1.addEventListener("click",
+            function(){
+            styleCSS.href= "shifumimod.css"
+            rock1.src="raised-fist_270a.png"
+            paper1.src="raised-hand_270b.png"
+            scissors1.src="victory-hand_270c.png"
+            rock0.src="raised-fist_270a.png"
+            paper0.src="raised-hand_270b.png"
+            scissors0.src="victory-hand_270c.png"
+            rock2.src="raised-fist_270a.png"
+            paper2.src="raised-hand_270b.png"
+            scissors2.src="victory-hand_270c.png"
+            lose.src="face-palm_1f926.png"
+            equal.src="shrug_1f937.png"
+            won.src="party-popper_1f389.png"
+            tooltip1.innerHTML="Rock wins over Scissors and loses against Paper"
+            tooltip2.innerHTML="Paper wins over Rock and loses against Scissors"
+            tooltip3.innerHTML="Scissors win over Paper and lose against Rock"
+        }
+        );
+        theme2.addEventListener("click",
+            function(){
+            styleCSS.href="shifumimod2.css"
+            rock1.src="bowser.png"
+            paper1.src="mario.png"
+            scissors1.src="peach.png"
+            rock0.src="bowser.png"
+            paper0.src="mario.png"
+            scissors0.src="peach.png"
+            rock2.src="bowser.png"
+            paper2.src="mario.png"
+            scissors2.src="peach.png"
+            lose.src="mariosad.png"
+            won.src="etoile.png"
+            equal.src="mariosad.png"
+            tooltip1.innerHTML="Bowser wins over Peach and loses against Mario"
+            tooltip2.innerHTML="Mario wins over Bowser and is weaken by Peach"
+            tooltip3.innerHTML="Peach weakens Mario and loses against Bowser"
+        }
+        );
+            }
+
+changeTheme();
 
     function erase(){
         rock1.style.display="none";
